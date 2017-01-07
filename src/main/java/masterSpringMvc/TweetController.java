f10e6d60515efbe3fb749e5e129a11eec7227f4d
Gus Long <mail@anguslong.net>
2017-01-07 09:36:05 +0100

    added an error on a bad search

--- src/main/java/masterSpringMvc/TweetController.java
+++ src/main/java/masterSpringMvc/TweetController.java
@@ -3,6 +3,8 @@ package masterSpringMvc;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.twitter.api.SearchResults;
 import org.springframework.social.twitter.api.Tweet;
@@ -10,7 +12,9 @@ import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class TweetController {
@@ -24,7 +28,7 @@ public class TweetController {
     	return "searchPage";
     }
     
-    // ability to search by passing a parameter eg  http://localhost:8080/?search=springMVC
+    // ability to search by passing a URL parameter eg  http://localhost:8080/?search=springMVC
     @RequestMapping("/result")
     public String hello(@RequestParam(defaultValue = "trump") String search, Model model) {
         SearchResults searchResults = twitter.searchOperations().search(search);
@@ -32,5 +36,19 @@ public class TweetController {
         model.addAttribute("tweets", tweets);
         model.addAttribute("search", search);
         return "resultPage";
+    }
+    
+    // redirects POST method to /result and passes in the search parameter
+    @RequestMapping(value = "/postSearch", method = RequestMethod.POST)
+    public String postSearch(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+        String search = request.getParameter("search");
+        
+        // check for forbidden search term
+        if (search.toLowerCase().contains("struts")) {
+        	redirectAttributes.addFlashAttribute("error", "Try using Spring instead");
+        	return "redirect:/";
+        }
+        redirectAttributes.addAttribute("search", search);
+        return "redirect:result";
     }
 }
